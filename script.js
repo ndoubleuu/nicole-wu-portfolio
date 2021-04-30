be89173f93b3f8e799bd13e2e8b94ce33640d46f
Nicole <nicolewu.ncw@gmail.com>
2021-04-30 01:35:24 -0400

    typing effect on h1 and h2

---
+++ script.js
@@ -0,0 +1,37 @@
+let i = 0;
+let n = 0;
+
+const textOne = `< Hello World! />`
+const textTwo = `I'm Nicole, and I'm a Front-end Developer.`
+
+const speed = 55;
+
+const partOne = document.querySelector('#partOne');
+
+const typingOne = () => {
+    if (i < textOne.length) {
+        //print the text onto the page
+            // This piece of code prints one letter after the other as an entire line of text
+        partOne.innerHTML += textOne.charAt(i);
+        //Make letters go from one to the next to create typing effect
+        i++; 
+        setTimeout(typingOne, speed);
+    }
+}
+
+const typingTwo = () => {
+    if (n < textTwo.length) {
+        partTwo.innerHTML += textTwo.charAt(n);
+        n++;
+        setTimeout(typingTwo, speed);
+    } 
+}
+
+// Delay the function call so that the typing doesn't happen until 1.5s after page has been loaded
+setTimeout(function() { 
+    typingOne();
+}, 1500);
+
+setTimeout(function() {
+    typingTwo();
+}, 2800);
