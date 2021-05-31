b544003d52ae5018ae8121b4acd2bc26e1823809
Nicole <nicolewu.ncw@gmail.com>
2021-05-31 01:18:19 -0400

    finished about me section and added scroll effects woop woop

--- script.js
+++ script.js
@@ -1,37 +1,41 @@
-let i = 0;
-let n = 0;
+// Initialize Animate On Scroll library
+AOS.init();
 
-const textOne = `< Hello World! />`
-const textTwo = `I'm Nicole, and I'm a`
+// let i = 0;
 
-const speed = 55;
+// let n = 0;
 
-const partOne = document.querySelector('#partOne');
+// const textOne = `< Hello World! />`
+// const textTwo = `I'm Nicole, and I'm a`
 
-const typingOne = () => {
-    if (i < textOne.length) {
-        //print the text onto the page
-        // This piece of code prints one letter after the other as an entire line of text
-        partOne.innerHTML += textOne.charAt(i);
-        //Make letters go from one to the next to create typing effect
-        i++;
-        setTimeout(typingOne, speed);
-    }
-}
+// const speed = 55;
 
-const typingTwo = () => {
-    if (n < textTwo.length) {
-        partTwo.innerHTML += textTwo.charAt(n);
-        n++;
-        setTimeout(typingTwo, speed);
-    }
-}
+// const partOne = document.querySelector('#partOne');
 
-// Delay the function call so that the typing doesn't happen until 1.5s after page has been loaded
-setTimeout(function () {
-    typingOne();
-}, 500);
+// const typingOne = () => {
+//     if (i < textOne.length) {
+//         //print the text onto the page
+//         // This piece of code prints one letter after the other as an entire line of text
+//         partOne.innerHTML += textOne.charAt(i);
+//         //Make letters go from one to the next to create typing effect
+//         i++;
+//         setTimeout(typingOne, speed);
+//     }
+// }
 
-setTimeout(function () {
-    typingTwo();
-}, 2000);
+// const typingTwo = () => {
+//     if (n < textTwo.length) {
+//         partTwo.innerHTML += textTwo.charAt(n);
+//         n++;
+//         setTimeout(typingTwo, speed);
+//     }
+// }
+
+// // Delay the function call so that the typing doesn't happen until 1.5s after page has been loaded
+// setTimeout(function () {
+//     typingOne();
+// }, 500);
+
+// setTimeout(function () {
+//     typingTwo();
+// }, 2000);
